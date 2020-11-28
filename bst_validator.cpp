b94d062d6b4ebef6dd93013e6ab5371a06ea2974
Peter Nguyen <ppn4@txstate.edu>
2020-11-28 05:15:30 +0000

    new func

--- bst_validator.cpp
+++ bst_validator.cpp
@@ -5,6 +5,123 @@
 
 using namespace std;
 
+struct Node 
+{ 
+    int data; 
+    Node* left;
+    Node* right; 
+}; 
+  
+Node* newNode(int data) 
+{ 
+    Node* node = (Node*)malloc(sizeof(Node)); 
+    node->data = data; 
+    node->left = node->right = NULL; 
+    return node; 
+} 
+  
+Node* insertLevelOrder(vector<int>& arr, Node* root, int i, int n) { 
+    // Base case for recursion 
+    if (i < n) 
+    { 
+        Node* temp = newNode(arr[i]); 
+        root = temp; 
+  
+        // insert left child 
+        root->left = insertLevelOrder(arr, root->left, 2 * i + 1, n); 
+  
+        // insert right child 
+        root->right = insertLevelOrder(arr, root->right, 2 * i + 2, n); 
+    } 
+    return root; 
+} 
+  
+void inOrder(Node* root) 
+{ 
+    if (root != NULL) { 
+        inOrder(root->left); 
+        cout << root->data <<" "; 
+        inOrder(root->right); 
+    } 
+}
+
+int isBSTUtil(Node* t, int min, int max)  {  
+	if (t == nullptr) {
+    return 1;
+  }
+		 
+		// cout<< "this is min1 " << min << endl;
+    // cout << "this is max1 " << max << endl;
+    // cout << "t->data1 " << t->data <<endl;
+
+	if (t->data < min || t->data > max){
+    // cout<< "this is min " << min << endl;
+    // cout << "this is max " << max << endl;
+    // cout << "t->data " << t->data << endl;
+    return 0;
+  } 
+		 
+	
+	return isBSTUtil(t->left, min, t->data - 1) && isBSTUtil(t->right, t->data + 1, max); 
+}  
+
+int isBST(Node* t, vector<int>& arr)  {  
+    
+    double smallest = arr[0] ;
+    for ( int i=1;  i < arr.size();  i++ ){
+        if ( arr[i] < smallest ){
+            smallest = arr[i];
+        }
+             
+    }
+    //cout << "min = "<< smallest << '\n' ;
+    double largest = arr[0] ;
+    for ( int i=1;  i < arr.size();  i++ ){
+        if ( arr[i] > largest ){
+          largest = arr[i] ;
+        }
+             
+    }
+    //cout << "min = "<< largest << '\n' ;
+      int min = smallest;
+      int max = largest;
+    
+    return(isBSTUtil(t, min, max));  
+}  
+  
+
+int main() 
+{ 
+  string mystr;
+  int tmp; 
+  //cout << "What are the numbers" << endl;
+  getline (cin, mystr);
+
+  stringstream str_strm(mystr);
+  vector<int> myvect; 
+  while (str_strm >> tmp) {
+      myvect.push_back(tmp);
+  }
+  //int n = sizeof(myvect)/sizeof(myvect[0]); 
+  int n = myvect.size();
+
+  Node *root;
+  for(int i = 0; i < myvect.size(); i++){
+    root = insertLevelOrder(myvect, root, 0, n); 
+  }
+
+  //inOrder(root);
+  //cout << endl;
+
+  if(isBST(root, myvect))  
+        cout<<"True";  
+    else
+        cout<<"False"; 
+
+} 
+
+
+/*
 bool validbst(int arr[],int n) {
         for(int i=0;i<n;i++) {
             int l= 2*i+1;   // left child
@@ -27,3 +144,4 @@ int main(){
 	 int n= sizeof(arr) / sizeof(arr[0]);
         cout<<validbst(arr,n);
 }
+*/
