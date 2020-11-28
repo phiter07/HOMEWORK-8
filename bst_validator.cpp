1ddcff8acdbdf627abec5d1432b9ee866f2bc085
Peter Nguyen <ppn4@txstate.edu>
2020-11-28 05:54:33 +0000

    xc

--- bst_validator.cpp
+++ bst_validator.cpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 bool validbst(int arr[],int n) {
-        for(int i=0;i<n;i++) {
-            int l= 2*i+1;   // left child
-            int r = 2*i+2;  //right child
-        if(l<n){
-          if(arr[l]>arr[i])//if left child greater then root then return false
-            return false;
-        }     
-         if( r<n ){
-             if(arr[r]<arr[i])//if right child less then root return flase
-                return false;
-        }}
-        return true;
-    }
 
+  for(int i=0;i<n;i++) {
+    int l= 2*i+1;   // left child
+    int r = 2*i+2;  //right child
+  if(l<n){
+    if(arr[l]>arr[i])//if left child greater then root then return false
+    return false;
+    }     
+  if( r<n ){
+    if(arr[r]<arr[i])//if right child less then root return flase
+    return false;
+    }
+  }
+  return true;
+}
 
 int main() {
 	 int arr[]={2,4,5};
