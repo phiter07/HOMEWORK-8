015a872760d3f15f0ecedf11660511cfaa6bc5f2
Peter Nguyen <ppn4@txstate.edu>
2020-11-30 04:51:56 +0000

    k

--- bst_lca.cpp
+++ bst_lca.cpp
@@ -1,99 +1,62 @@
-#include <iostream> 
-#include <string> 
-#include <sstream> 
-#include <vector>
-
+#include <iostream>
 using namespace std;
-
-class BST {
-    
-    struct node {
-        int data;
-        node* left;
-        node* right;
-    };
-
-    node* root;
-
-    node* insert(int x, node* t)
-    {
-        if(t == NULL)
-        {
-            t = new node;
-            t->data = x;
-            t->left = t->right = NULL;
-        }
-        else if(x < t->data)
-            t->left = insert(x, t->left);
-        else if(x > t->data)
-            t->right = insert(x, t->right);
-        return t;
-    }
-
-    void inorder(node* t) {
-      if(t == NULL)
-            return;
-        inorder(t->left);
-        cout << t->data << " ";
-        inorder(t->right);
-    }
-    node* LCA(node* root, int n1, int n2) {
-      if (root == NULL){
-        return NULL;
-    }
-      
-      if (root->data > n1 && root->data > n2){
-        return LCA(root->left, n1, n2);
-    }
-      
-      if (root->data< n1 && root->data < n2){
-        return LCA(root->right, n1, n2);
-    }
-      
-    return root;
-}
-
-
-public:
-    BST() {
-        root = NULL;
-    }
-
-    void insert(int x) {
-        root = insert(x, root);
-    }
-
-    void display() {
-        inorder(root);
-        cout << endl;
-    }
-    void lowestcommon(int n1, int n2){
-      root = LCA(root, n1, n2);
-      cout << "lca = " << root->data;
-    }
-
+struct Node {
+  int data;
+  Node* left;
+  Node* right;
 };
 
-int main() 
-{ 
-  BST t;
-  string mystr;
-  double n1, n2;
-  int tmp; 
-  
-  getline (cin, mystr);
-  cin >> n1;
-  cin >> n2;
+Node* CreateNode(int data) {
+  Node* new_node = new Node();
+  new_node->data = data;     
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
 
-  stringstream str_strm(mystr);
-  vector<int> myvect; 
-  while (str_strm >> tmp) {
-      myvect.push_back(tmp);
-  }
+Node* addNode(Node* root, int data) {
+  if(root == NULL){
+    root = CreateNode(data);}
+  else if(data <= root->data)
+    root->left = addNode(root->left, data);
+  else root->right = addNode(root->right, data);
+
+  return root;
+}
+int lca(Node* root, int n1, int n2)  
+{  
+    if (root == NULL) return -1;  
   
-  for(int i =0; i < myvect.size(); i++){
-    t.insert(myvect[i]);
-  }
-  t.lowestcommon(n1, n2);
-	 
-} 
+    // If both n1 and n2 are smaller 
+    // than root, then LCA lies in left  
+    if (root->data > n1 && root->data > n2)  
+        return lca(root->left, n1, n2);  
+  
+    // If both n1 and n2 are greater than  
+    // root, then LCA lies in right  
+    if (root->data < n1 && root->data < n2)  
+        return lca(root->right, n1, n2);  
+  
+    return root->data;  
+}  
+
+
+int main () {
+  Node* root = NULL;
+
+  int sum=0;
+  int arr[] = {10,5,15,2,7,11,25,1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  for(int i = 0; i < size; i++) {
+     std::cout<<arr[i]<<" ";
+    root = addNode(root, arr[i]);
+ 	}
+    std::cout<<endl;
+ 	int n1,n2;
+ 	n1=11; n2=25;
+    std::cout<<n1<<endl;
+     std::cout<<n2<<endl;
+ std::cout<<"lca ="<<lca(root,n1,n2);	
+  
+}
