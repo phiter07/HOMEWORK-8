376155a31dad269569ca7d5b5bfd69ce703d52d3
Peter Nguyen <ppn4@txstate.edu>
2020-11-30 05:13:25 +0000

    d

--- bst_validator.cpp
+++ bst_validator.cpp
@@ -1,31 +1,38 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-bool validbst(int arr[],int n){
-  for(int i=0;i<n;i++){
+
+bool validbst(int arr[], int n) {
+  for (int i=0; i<n; i++) {
     int l= 2*i+1;// left chile
     int r = 2*i+2;//right child
-    f(l<n) {
-      if(arr[l]>arr[i])//if left child greater then root then return false
-      return false;
-    }      
-    if( r<n ){
-      if(arr[r]<arr[i])//if right child less then root return flase
-      return false;
-        }
+  
+  if(l<n) {
+    if(arr[l]>arr[i])
+    return false;
   }
-        return true;
+  if(r<n) {
+    if(arr[r]<arr[i])
+    return false;
+  }
+  }
+  return true;
 }
+
 int main(){
-	 int arr[]={10,5,15,3,8,12,25};
-	 int n= sizeof(arr) / sizeof(arr[0]);
-   for(int i = 0; i < n; i++) {
-    //std::cout<<arr[i]<<" ";
-   
- 	}
-    std::cout<<endl;
-       int chk=validbst(arr,n);
-        if(chk==1)
-        std::cout<<"True";
-        else 
-        std::cout<<"False";
+  int arr[]={2,4,5};
+  //int arr[]={10,5,15,3,8,12,25};
+
+  int n= sizeof(arr) / sizeof(arr[0]);
+  
+  for(int i = 0; i < n; i++) {
+    std::cout<<arr[i]<<" ";
+  }
+
+  std::cout<<endl;
+  int chk=validbst(arr,n);
+  if(chk==1)
+  std::cout<<"True";
+  else
+  std::cout<<"False";
 }
