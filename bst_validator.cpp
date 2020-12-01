65df19750b4f9814c609865debac5fd753e125ed
Peter Nguyen <ppn4@txstate.edu>
2020-12-01 00:31:58 +0000

    function

--- bst_validator.cpp
+++ bst_validator.cpp
@@ -1,38 +1,42 @@
-#include <iostream>
-
+#include<iostream>
+#include <sstream>
 using namespace std;
-
-bool validbst(int arr[], int n) {
-  for (int i=0; i<n; i++) {
-    int l= 2*i+1;// left chile
-    int r = 2*i+2;//right child
-  
-  if(l<n) {
-    if(arr[l]>arr[i])
-    return false;
-  }
-  if(r<n) {
-    if(arr[r]<arr[i])
-    return false;
-  }
-  }
-  return true;
-}
-
+bool validbst(int arr[],int n){
+    
+        for(int i=0;i<n;i++)
+        {
+            
+            int l= 2*i+1;// left chile
+            int r = 2*i+2;//right child
+        if( l<n)
+            {if(arr[l]>arr[i])//if left child greater then root then return false
+            return false;
+        }
+                
+         if( r<n ){
+             if(arr[r]<arr[i])//if right child less then root return flase
+                return false;
+        }}
+        return true;
+    }
 int main(){
-  int arr[]={2,4,5};
-  //int arr[]={10,5,15,3,8,12,25};
-
-  int n= sizeof(arr) / sizeof(arr[0]);
-  
-  for(int i = 0; i < n; i++) {
-    std::cout<<arr[i]<<" ";
-  }
-
-  std::cout<<endl;
-  int chk=validbst(arr,n);
-  if(chk==1)
-  std::cout<<"True";
-  else
-  std::cout<<"False";
+	
+	 int arr[1000];
+	int i=0;
+   string line;int enterNumber;
+getline(cin, line);
+istringstream iss(line);
+while (iss >> enterNumber)
+{
+    //numbers.push_back(enterNumber);
+     arr[i]=(int) enterNumber;
+     i++;
+}
+ 	
+       int chk=validbst(arr,i);
+     
+        if(chk==1)
+        std::cout<<"True";
+        else 
+        std::cout<<"False";
 }
